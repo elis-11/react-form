a1804358a0b048d994d5037d4428799fc2882788
elis-11 <eliza.arzanukaeva@gmail.com>
2021-09-20 21:56:17 +0200

    const setValues = (values) => {

--- src/components/DataContext.js
+++ src/components/DataContext.js
@@ -4,4 +4,8 @@ const DataContext = createContext();
 
 export const DataProvider =({ children }) =>{
     const [data, setData]=useState({})
+
+    const setValues = (values) => {
+        
+    }
 }
