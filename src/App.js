7a8b26b6b3d623a8a1eb6186eeacf40ec33c825b
elis-11 <eliza.arzanukaeva@gmail.com>
2021-09-20 20:49:26 +0200

    const counter = () => {

--- src/App.js
+++ src/App.js
@@ -1,61 +1,17 @@
-import { useState } from "react";
-import "./App.css";
-import CounterComp from "./components/CounterComp";
-import ResultComp from "./components/ResultComp";
+import './App.css'
+
+const counter = () => {
+  const [counter, setCounter] = useState(0);
+};
+
+
 
 function App() {
-  const [counter1, setCounter1] = useState(0);
-  const increaseCounter1 = () => {
-    setCounter1(counter1 + 1);
-  };
-  const decreaseCounter1 = () => {
-    setCounter1(counter1 - 1);
-  };
-  const [counter2, setCounter2] = useState(0);
-  const increaseCounter2 = () => {
-    setCounter2(counter2 + 1);
-  };
-  const decreaseCounter2 = () => {
-    setCounter2(counter2 - 1);
-  };
-  const addResult = counter1 + counter2;
-  const subResult = counter1 - counter2;
-  const multResult = counter1 * counter2;
-  const divResult = counter1 / counter2;
-
-  const totalResult = addResult + divResult + multResult + subResult;
-
   return (
     <div className="App">
-      <div>
-        <CounterComp
-          increaseCount={increaseCounter1}
-          decreaseCount={decreaseCounter1}
-          count={counter1}
-        />
-        <CounterComp
-          increaseCount={increaseCounter2}
-          decreaseCount={decreaseCounter2}
-          count={counter2}
-        />
-      </div>
-      <div>
-        <ResultComp title="Addition" result={addResult} />
-      </div>
-      <div>
-        <ResultComp title="Substraction" result={subResult} />
-      </div>
-      <div>
-        <ResultComp title="Multiplication" result={multResult} />
-      </div>
-      <div>
-        <ResultComp title="Division" result={divResult} />
-      </div>
-      <div className="total">
-        <ResultComp title="Total Result" result={totalResult} />
-      </div>
+      <h4>You clicked {counter} times</h4>
+     
     </div>
   );
 }
-
 export default App;
