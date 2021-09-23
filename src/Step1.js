5ba95089a3249c3e7bde7bb045132abff80ca17e
elis-11 <eliza.arzanukaeva@gmail.com>
2021-09-23 18:36:30 +0200

    <Container container='main' maxWidth='xs'>

--- src/Step1.js
+++ src/Step1.js
@@ -1,5 +1,9 @@
 import React from 'react';
+import { MainContainer } from './components/MainContainer';
 
 export const Step1=()=>{
+    <MainContainer >
+        
+    </MainContainer>
     return 
 }
--- src/components/MainContainer.js
+++ src/components/MainContainer.js
@@ -1,6 +1,6 @@
 import React from 'react';
 
-export const MainContainer =({ children, ...props });=> {
+export const MainContainer =({ children, ...props }) => {
     return (
         <Container container='main' maxWidth='xs'>
             {children}
