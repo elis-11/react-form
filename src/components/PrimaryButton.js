9acd897a5b98fb708d96b984a739c86cf0fada03
elis-11 <eliza.arzanukaeva@gmail.com>
2021-09-23 20:28:05 +0200

    export const PrimaryButton = ({ children, props }) => {

--- src/components/PrimaryButton.js
+++ src/components/PrimaryButton.js
@@ -1,1 +1,26 @@
-import React from 'react';
+import React from "react";
+import { Button } from "@material-ui/core";
+import { makeStyles } from "@material-ui/core/styles";
+
+const useStyles = makeStyles((theme) => ({
+  root: {
+    margin: theme.spacing(3, 0, 2),
+  },
+}));
+
+export const PrimaryButton = ({ children, props }) => {
+  const styles = useStyles();
+
+  return (
+    <Button
+      className={styles.root}
+      type="submit"
+      fullwidth
+      variant="contained"
+      color="primary"
+      {...props}
+    >
+      {children}
+    </Button>
+  );
+};
