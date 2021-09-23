59970514b2535c0f02edb5d0293dc7601a14d789
elis-11 <eliza.arzanukaeva@gmail.com>
2021-09-23 21:58:28 +0200

    <Route exact path="/" component={Step1} />

--- src/App.js
+++ src/App.js
@@ -1,9 +1,10 @@
-import {Step1} from './Step1'
+import {Step} from './Step1'
 // import { Step } from 'material-ui';
 import React from 'react';
 import { BrowserRouter as Router, Switch, Route } from 'react-router-dom';
 import { Header } from './components/Header';
 
+// const Step1 =() => <>Step 1</>;
 const Step2 =() => <>Step 2</>;
 const Step3 =() => <>Step 3</>;
 const Result =() => <>Result</>;
@@ -15,7 +16,7 @@ function App() {
 <Header />
 <Router>
   <Switch>
-    <Route exact path="/" component={Step1} />
+    <Route exact path="/" component={Step} />
     <Route  path="/step2" component={Step2} />
     <Route  path="/step3" component={Step3} />
     <Route  path="/result" component={Result} />
