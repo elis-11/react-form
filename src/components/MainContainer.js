c80ec90db28d021bed8cec62e422630816048285
elis-11 <eliza.arzanukaeva@gmail.com>
2021-09-23 18:44:29 +0200

    export const MainContainer =({ children, ...props }) => {

--- src/components/MainContainer.js
+++ src/components/MainContainer.js
@@ -1,8 +1,20 @@
 import React from 'react';
+import {makeStyles} from "@material-ui/core/styles"
+
+const useStyles = makeStyles((theme)=>({
+    root: {
+        marginTop: theme.spacing(4),
+        display: 'flex',
+        flexDirection: 'column',
+        alignItems: 'center',
+    },
+}))
 
 export const MainContainer =({ children, ...props }) => {
+const styles =useStyles()
+
     return (
-        <Container container='main' maxWidth='xs'>
+        <Container className={styles.root} container='main' maxWidth='xs' {...props}>    
             {children}
         </Container>
     )
