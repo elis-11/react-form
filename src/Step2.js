213ecbe6238b6a94eabc5b44a440ea7cda7b283c
elis-11 <eliza.arzanukaeva@gmail.com>
2021-09-24 19:23:54 +0200

    const { register, handleSubmit, errors, watch } = useForm({

--- src/Step2.js
+++ src/Step2.js
@@ -1,15 +1,25 @@
-import Typography from "material-ui/styles/typography";
+import { Typography } from "material-ui/core";
+import { useForm } from "react-hook-form";
+import { useHistory } from "react-router-dom";
 import { Form } from "./components/Form";
 import { Input } from "./components/Input";
 import { MainContainer } from "./components/MainContainer";
-import { useForm } from 'react-hook-form';
-import { useHistory } from "react-router";
+import { PrimaryButton } from "./components/PrimaryButton";
+import * as yup from "yup";
+import { yupResolver } from "@hookform/resolvers";
 
+const schema = yup.object().shape({
+  email: yup
+    .string()
+    .email("Email should have correct format")
+    .required("Email is a required field"),
+});
 
 export const step2 = () => {
   const history = useHistory();
   const { register, handleSubmit, errors, watch } = useForm({
     mode: "onBlur",
+    resolver: yupResolver(schema),
   });
 
   const onSubmit = (data) => {
@@ -22,7 +32,16 @@ export const step2 = () => {
         Step 2
       </Typography>
       <Form>
-        <Input id="email" type="email" label="Email" name="email" required />
+        <Input
+          id="email"
+          type="email"
+          label="Email"
+          name="email"
+          required
+          error={!!errors.email}
+          helperText={!!errors?.email?.message}
+        />
+        <PrimaryButton>Next</PrimaryButton>
       </Form>
     </MainContainer>
   );
