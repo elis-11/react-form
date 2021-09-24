072a960f85a842f222ce331e61911f63ab885f7b
elis-11 <eliza.arzanukaeva@gmail.com>
2021-09-24 18:55:10 +0200

    const history = useHistory;

--- src/Step2.js
+++ src/Step2.js
@@ -1,14 +1,26 @@
-import Typography from "material-ui/styles/typography"
-import { Form } from "./components/Form"
-import { MainContainer } from "./components/MainContainer"
+import Typography from "material-ui/styles/typography";
+import { Form } from "./components/Form";
+import { Input } from "./components/Input";
+import { MainContainer } from "./components/MainContainer";
 
-export const step2=()=>{
-    return <MainContainer>
-        <Typography component='h2' variant='h5'>
-            Step 2
-        </Typography>
-        <Form>
-            <Input
-        </Form>
+export const step2 = () => {
+  const history = useHistory;
+  const { register, handleSubmit, errors, watch } = useForm({
+    mode: "onBlur",
+  });
+
+  const onSubmit = (data) => {
+    history.push("/step3");
+  };
+
+  return (
+    <MainContainer>
+      <Typography component="h2" variant="h5">
+        Step 2
+      </Typography>
+      <Form>
+        <Input id="email" type="email" label="Email" name="email" required />
+      </Form>
     </MainContainer>
-}
+  );
+};
