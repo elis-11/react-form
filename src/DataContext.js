d382d31c465c1cc1ea875e25145835f21175bae8
elis-11 <eliza.arzanukaeva@gmail.com>
2021-09-25 15:12:05 +0200

    document.getElementById("root")

--- src/DataContext.js
+++ src/DataContext.js
@@ -1,21 +1,20 @@
-import React, { createContext, useContext, useState } from "react";
+import React, { createContext, useState, useContext } from 'react'
 
-const DataContext = createContext();
+const DataContext = createContext()
 
-export const DataProvider = ({ children }) => {
+export const DataProvider = ({children}) => {
   const [data, setData] = useState({});
 
   const setValues = (values) => {
-    setData((prevData) => ({
+    setData(prevData => ({
       ...prevData,
-      ...values,
-    }));
-  };
-  return (
-    <DataContext.Provider value={{ data, setValues }}>
-      {children}
-    </DataContext.Provider>
-  );
-};
+      ...values
+    }))
+  }
 
-export const useData = () => useContext(DataContext);
+  return <DataContext.Provider value={{data, setValues}}>
+    {children}
+  </DataContext.Provider>
+}
+
+export const useData = () => useContext(DataContext)
