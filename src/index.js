da11d21efac695e9a21507d6ac5d1deda0c0d96e
elis-11 <eliza.arzanukaeva@gmail.com>
2021-09-26 10:05:38 +0200

    import * as serviceWorker from "./serviceWorker";

--- src/index.js
+++ src/index.js
@@ -3,6 +3,8 @@ import ReactDOM from "react-dom";
 import './index.css'
 import App from "./App";
 import { DataProvider } from "./DataContext";
+import * as serviceWorker from "./serviceWorker";
+
 
 ReactDOM.render(
   <React.StrictMode>
@@ -12,3 +14,7 @@ ReactDOM.render(
   </React.StrictMode>,
   document.getElementById("root")
 );
+// If you want your app to work offline and load faster, you can change
+// unregister() to register() below. Note this comes with some pitfalls.
+// Learn more about service workers: https://bit.ly/CRA-PWA
+serviceWorker.unregister();
